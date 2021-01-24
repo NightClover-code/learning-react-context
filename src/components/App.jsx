7c3592381175697694bf5243b7dd6ffea6132069
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-01-25 00:11:48 +0100

    added components Field, Button & UserCreate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
-
-const App = () => {
-  return <div>App</div>;
-};
-
+import React, { Component } from 'react';
+//importing components
+import UserCreate from './UserCreate';
+//app component
+class App extends Component {
+  //component level state
+  state = { language: 'english' };
+  //changing language
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          ></i>
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          ></i>
+        </div>
+        <UserCreate />
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/components/UserCreate.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//improting components
+import Button from './Button';
+import Field from './Field';
+//user create component
+const UserCreate = () => {
+  return (
+    <div className="ui form">
+      <Field />
+      <Button />
+    </div>
+  );
+};
+export default UserCreate;
